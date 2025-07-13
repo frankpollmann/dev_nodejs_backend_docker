d0cc0bb2f23e608671e59df2a7b8de6a24b6638d
Frank Pollmann <frank.pollmann@outlook.de>
2025-07-13 22:50:39 +0200

    Delete backend/generate-swagger.js

--- backend/generate-swagger.js
+++
@@ -1,6 +0,0 @@
-// scripts/generate-swagger.js
-const fs = require('fs');
-const swaggerSpec = require('../src/swagger'); // oder wo du swagger.js hast
-
-fs.writeFileSync('./swagger-output.json', JSON.stringify(swaggerSpec, null, 2));
-console.log('📄 Swagger-Doku wurde generiert: swagger-output.json');
